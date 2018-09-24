84e83c285bcfe9b8b897e2f61038e2971dfea84a
MiChel Causey <michel.causey@gmail.com>
2018-09-24 19:28:42 -0400

    added a hide function, moved answers into an array instead of object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,150 +1,70 @@
 var answers = [];
+var correctAnswers = ["c", "b", "c", "d", "a", "c", "d", "c", "b", "b", "c", "a"]
+var questionNumber = 0
 
 // an object array to hold my questions & answer choices, and correct answers.
 var quizContent = [{
         question: "'The Reaping' portrays which biblical events?",
-        answers: {
-            a: "The Destruction of Sodom & Gomorrah",
-            b: "The Great Flood",
-            c: "The Plagues of Egypt",
-            d: "David vs. Goliath"
-        },
-
-        correctAnswer: "c"
-
+        answers: ["The Destruction of Sodom & Gomorrah<br>", "The Great Flood<br>", "The Plagues of Egypt<br>", "David vs. Goliath<br>"]
     },
     {
         question: "Select the correct character name / actress combo for the mom in 'The Ring', 'The Ring 2', and 'Rings.'",
-        answers: {
-            a: "Margaret White / Piper Laurie",
-            b: "Rachel Keller / Naomi Watts",
-            c: "Lynn Sear / Toni Collette",
-            d: "Annie Wilkes / Kathy Bates",
-        },
-
-        correctAnswer: "b"
+        answers: ["A: Margaret White / Piper Laurie", "B: Rachel Keller / Naomi Watts", "C: Lynn Sear / Toni Collette", "D: Annie Wilkes / Kathy Bates"]
     },
     {
         question: "As of September 2018, how many movies make up the Paranormal Activity series?",
-        answers: {
-            a: "4",
-            b: "5",
-            c: "6",
-            d: "7",
-        },
-
-        correctAnswer: "c"
+        answers: ["A: 4", "B: 5", "C: 6", "D: 7"]
     },
     {
         question: "Which of the following characters are NOT a member of 'The Losers Club' in Stephen King's IT?",
-        answers: {
-            a: "Richard Tozier",
-            b: "Bill Denbrough",
-            c: "Beverly Marsh",
-            d: "Henry Bowers",
-        },
-
-        correctAnswer: "d"
+        answers: ["A: Richard Tozier", "B: Bill Denbrough", "C: Beverly Marsh", "D: Henry Bowers"]
     },
     {
         question: "In 'The Strangers', who does the girl in the blonde mask ask for at the door, during her initial contact with Kristen & James?",
-        answers: {
-            a: "Tamara",
-            b: "Tanya",
-            c: "Terrie",
-            d: "Tabatha",
-        },
-
-        correctAnswer: "a"
+        answers: ["Tamara", "Tanya", "Terrie", "Tabatha", ]
     },
     {
         question: "Darkness Falls tells the revenge story of which magical creature?",
-        answers: {
-            a: "Jack Frost",
-            b: "Slenderman",
-            c: "The Toothfairy",
-            d: "Belsnickel",
-        },
-
-        correctAnswer: "c"
+        answers: ["Jack Frost", "Slenderman", "The Toothfairy", "Belsnickel"]
     },
     {
         question: "In what US City can you see the stairs that Damien fell down in The Exorcist?",
-        answers: {
-            a: "Mount Hood, Oregon",
-            b: "Burkittsville, Maryland",
-            c: "Vacherie, Louisiana",
-            d: "Georgetown, Washington DC",
-        },
-
-        correctAnswer: "d"
+        answers: ["Mount Hood, Oregon", "Burkittsville, Maryland", "Vacherie, Louisiana", "Georgetown, Washington DC"]
     },
     {
         question: "What fictional Maine town is the setting for Stephen King's IT?",
-        answers: {
-            a: "Castle Rock",
-            b: "Jerusalem's Lot",
-            c: "Derry",
-            d: "Ogunquit",
-        },
-
-        correctAnswer: "c"
+        answers: ["Castle Rock", "Jerusalem's Lot", "Derry", "Ogunquit"]
     },
     {
         question: "What is the correct chronological of the movies in 'The Conjuring' universe?",
-        answers: {
-            a: "Annabelle, The Conjuring, The Nun, The Conjuring 2",
-            b: "The Nun, Annabelle, The Conjuring, The Conjuring 2",
-            c: "The Conjuring, The Conjuring 2, Annabelle, The Nun",
-            d: "The Conjuring, The Nun, The Conjuring 2, Annabelle",
-        },
-
-        correctAnswer: "b"
+        answers: ["Annabelle, The Conjuring, The Nun, The Conjuring 2", "The Nun, Annabelle, The Conjuring, The Conjuring 2", "The Conjuring, The Conjuring 2, Annabelle, The Nun", "The Conjuring, The Nun, The Conjuring 2, Annabelle"]
     },
     {
         question: "In the movie Oculus, Kaylie begins eating an apple only to realize it's actually a(n):",
-        answers: {
-            a: "Eyeball",
-            b: "Lightbulb",
-            c: "Onion",
-            d: "Heart",
-        },
-
-        correctAnswer: "b"
+        answers: ["Eyeball", "Lightbulb", "Onion", "Heart"]
     },
     {
         question: "Who spent their summer at Camp Crystal Lake?",
-        answers: {
-            a: "Freddy",
-            b: "Michael",
-            c: "Jason", //JSON
-            d: "Johnny",
-        },
-
-        correctAnswer: "c"
+        answers: ["Freddy", "Michael", "Jason", "Johnny"]
     },
     {
         question: "The five main characters in Cabin in the Woods are associated with a specific trope. Which one is missing from the list: The Virgin, The Whore, The Fool, The Scholar, and: ",
-        answers: {
-            a: "The Athlete",
-            b: "The Healer",
-            c: "The Protector",
-            d: "The Savior",
-        },
-
-        correctAnswer: "a"
-    }
+        answers: ["The Athlete", "The Healer", "The Protector", "The Savior"]
+    },
 ]
 
 console.log(quizContent)
 
 function startGame() {
-    confirm("ready?")
-    if (confirm = true) {
+    $("#questions-div").hide()
+    $("#answers-div").hide()
+    $("#results").hide()
+    $("#button").on("click", function () {
+        $("#button").hide()
         run()
-    }
+        Quiz()
+    })
 }
-
 // set the clock to 30 seconds
 var timerCount = 30;
 var intervalID;
@@ -160,10 +80,8 @@ function decrement() {
     timerCount--;
     $("#timer").html("<h2>Time Remaining: " + timerCount + "</h2>")
 
-// display a warning at 15 seconds and stops the time when it reaches 0
-    if (timerCount === 15) {
-        $("#timer").html("<h2>Half-way! " + timerCount + "seconds remaining</h2>")
-    } else if (timerCount === 0) {
+    // display a warning at 15 seconds and stops the time when it reaches 0
+    if (timerCount === 0) {
         $("#timer").html("<h2>Time is Up!</h2>")
         stop()
     }
@@ -176,18 +94,20 @@ function stop() {
 
 function Quiz() {
 
-for (var i = 0; i < quizContent.length; i++){
-    console.log(quizContent)
-    $("#question-div").text(JSON.stringify(quizContent[0].question[0]))
-    $("#answer-div").text(JSON.stringify(quizContent[0].answers[0]))
-    i++
-}
+    $("#questions-div").show()
+    $("#answers-div").show()
 
-// the first question should appear with four answer choices
-// user will select their answer A, B, C, or D
-// store the answer to determine correct/incorrect answers
-// if the user does not answer the question before the timer reaches 0, the question is INCORRECT
-// if the user submits an answer before the timer runs out, the next question is posted
+    for (var i = 0; i < quizContent[questionNumber].answers.length; i++) {
+        $("#questions-div").html("<h2> Question: " + quizContent[questionNumber].question + "</h2>")
+        $("#answers-div").html("<h2> Answer Choices:<br> " + quizContent[questionNumber].answers + "</h2>")
+        questionNumber++
+    }
+
+    // the first question should appear with four answer choices
+    // user will select their answer A, B, C, or D
+    // store the answer to determine correct/incorrect answers
+    // if the user does not answer the question before the timer reaches 0, the question is INCORRECT
+    // if the user submits an answer before the timer runs out, the next question is posted
 
 }
 
@@ -197,4 +117,3 @@ function score() {
 }
 
 startGame()
-Quiz()
