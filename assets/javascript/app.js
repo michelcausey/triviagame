bd5446cea6421511a3a42c36b4e1b8b96446173a
MiChel Causey <michel.causey@gmail.com>
2018-09-27 20:27:26 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,58 +10,56 @@ $("#button").on("click", function () {
 
 })
 
-$("#submit").on("click", function () { // when user clicks the submit button, it will stop the clock & score
-    clearInterval(intervalID);
-    $("#questions-div").hide()
-    stop()
-    score()
-})
-
 $(document).ready(function () {
-    Quiz()
+            Quiz()
 
-    // set the clock to 90 seconds
-    var timerCount = 90;
-    var intervalID;
+            // set the clock to 90 seconds
+            var timerCount = 90;
+            var intervalID;
 
-    // ensures the timer starts over each time it runs down from 30 seconds
-    function run() {
-        clearInterval(intervalID);
-        intervalID = setInterval(decrement, 1000)
-    }
+            // ensures the timer starts over each time it runs down from 30 seconds
+            function run() {
+                clearInterval(intervalID);
+                intervalID = setInterval(decrement, 1000)
+            }
 
-    // function that counts down by 1 second intervals
-    function decrement() {
-        timerCount--;
-        $("#timer").html("<h2>You have " + timerCount + " seconds left.</h2>")
+            // function that counts down by 1 second intervals
+            function decrement() {
+                timerCount--;
+                $("#timer").html("<h2>You have " + timerCount + " seconds left.</h2>")
 
-        if (timerCount === 0) {
-            $("#timer").html("<h2>Time is Up!</h2>")
-            stop()
-        }
-    }
+                if (timerCount === 0) {
+                    $("#timer").html("<h2>Time is Up!</h2>")
+                    stop()
+                }
+            }
 
-    function stop() {
-            clearInterval(intervalID);
-            $("#questions-div").hide()
+            function stop() {
+                clearInterval(intervalID);
+                $("#questions-div").hide()
+                score()
+            }
+
+            function Quiz() {
+                run()
+                $('input').click(function () {
+                    if ($(this).is(':checked') && $(this).hasClass("correct")) {
+                        correctAnswers++;
+                    } else if ($(this).is(':checked') && $(this).hasClass("incorrect")) {
+                        incorrectAnswers++;
+                    }
+                })
+            }
+
+            function score() {
+                $("#submit").on("click", function () { // when user clicks the submit button, it will stop the clock & score
+                        clearInterval(intervalID);
+                        $("#questions-div").hide()
+                        $("#results").show()
+                        $("#correct-answers").html("<h2># of correct answers: " + correctAnswers + "</h2>")
+                        $("#incorrect-answers").html("<h2># of correct answers: " + incorrectAnswers + "</h2>")
+                    })
+                }
+            })
+
             score()
-    }
-
-    function Quiz() {
-        run()
-        $('input').click(function () {
-            if ($(this).is(':checked') && $(this).hasClass("correct")) {
-                correctAnswers++;
-            } else if ($(this).is(':checked') && $(this).hasClass("incorrect")) {
-                incorrectAnswers++;
-            } 
-        })
-    }
-
-    function score() {
-        $("#results").show()
-        $("#correct-answers").html("<h2># of correct answers: " + correctAnswers + "</h2>")
-        $("#incorrect-answers").html("<h2># of correct answers: " + incorrectAnswers + "</h2>")
-    }
-
-})
